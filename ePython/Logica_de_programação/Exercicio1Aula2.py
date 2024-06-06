7bb0d8a73791f39ed0a80f5c462f27470f377565
ViniciosBerudio <berudiovinicios@gmail.com>
2024-06-06 16:32:06 -0300

    add logica de progração

---
+++ ePython/Logica_de_programação/Exercicio1Aula2.py
@@ -0,0 +1,4 @@
+numero1 = int (input ("Digite um numero: "))
+numero2 = int (input ("Digite mais um numero: "))
+res = f"O resultado da soma de {numero1} com {numero2} é  {numero1 + numero2}."
+print(res)
