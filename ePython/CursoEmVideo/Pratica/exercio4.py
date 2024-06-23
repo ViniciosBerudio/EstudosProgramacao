aa3492d6f12bc1c5bfa57686c4d4d60a55926c35
ViniciosBerudio <berudiovinicios@gmail.com>
2024-06-23 14:49:21 -0300

    modificação no exercicio 4 (resolvido)

--- ePython/CursoEmVideo/Pratica/exercio4.py
+++ ePython/CursoEmVideo/Pratica/exercio4.py
@@ -2,6 +2,12 @@
 Faça um programa que leia algo pelo teclado e mostre na tela seu tipo primitivo e todas as informações possiveis sobre eles.
 
 """
-algo = input('Mostra algo na tela: ')
-print (type(algo))
-print (algo.isalpha())
+vari1 = input('Digite algo: ')
+print (type(vari1))
+print (f'E alfabetico? {vari1.isalpha()}')
+print (f'Só tem espaço? {vari1.isspace()}')
+print (f'É um numero? {vari1.isnumeric()}')
+print (f'É um alfanumerico? {vari1.isalnum()}')
+print (f'Está em maiscula? {vari1.isupper()}')
+print (f'Está em minuscula? {vari1.islower()}')
+print (f'Está  capitalizada? {vari1.istitle()}')
