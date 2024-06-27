9a1c486dc279517729c322f37ec976d79457808a
ViniciosBerudio <berudiovinicios@gmail.com>
2024-06-26 23:54:36 -0300

    ....

--- ePython/CursoEmVideo/Pratica/exercicio10.py
+++ ePython/CursoEmVideo/Pratica/exercicio10.py
@@ -2,7 +2,7 @@
 Crie um programa que leia quanto dinheiro tem na carteira e mostre o tanto de dolar que ela consegue comprar.  considere que o dolar = 5
 '''
 carteira = float(input('quando de dinheiro você tem na carteira? '))
-valor_dolar = 5
+valor_dolar = 3.27
 
 dolares = carteira / valor_dolar
 
--- ePython/CursoEmVideo/Pratica/exercicio8.py
+++ ePython/CursoEmVideo/Pratica/exercicio8.py
@@ -3,10 +3,10 @@ Escreva um programa que leia o valor em metros e o exiba em centimetros e milimi
 '''
 metro = float(input('Digite o valor em metros: '))
 
-centimetro = metro * 100
-milimetro = metro * 1000
+cm = metro * 100
+mm = metro * 1000
 
 print(f'====== Conversão de medidas ======')
 print(f'Valor em metros: {metro} m')
-print(f'Valor em centimetros: {centimetro} cm')
-print(f'Valor em milimetros: {milimetro} mm')
+print(f'Valor em centimetros: {cm} cm')
+print(f'Valor em milimetros: {mm} mm')
